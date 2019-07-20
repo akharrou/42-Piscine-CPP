5c29f87ec1ee98131e870ce87b08181f4cb4b558
Aymen Kh <idev.aymen@gmail.com>
2019-07-20 13:39:44 -0700

    Latest Update

---
+++ Day 04/ex04/main.cpp
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akharrou <akharrou@student.42.us.org>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/07/20 13:36:14 by akharrou          #+#    #+#             */
+/*   Updated: 2019/07/20 13:39:05 by akharrou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+#include <iostream>
+
+#include "IMiningLaser.hpp"
+#include "IAsteroid.hpp"
+
+#include "AsteroKreog.hpp"
+#include "DeepCoreMiner.hpp"
+#include "KoalaSteroid.hpp"
+#include "MiningBarge.hpp"
+#include "StripMiner.hpp"
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+int	main() {
+
+	{
+
+	}
+
+	std::cout << "\n/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */\n\n";
+
+	{
+
+	}
+
+	return (0);
+}
