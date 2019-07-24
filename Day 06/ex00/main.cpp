df40d6c4e1f222c6d19c36fb684ff5faa640a3b8
Aymen Kh <idev.aymen@gmail.com>
2019-07-23 23:03:38 -0700

    Latest Update

--- Day 06/ex00/main.cpp
+++ Day 06/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: akharrou <akharrou@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/23 19:22:36 by akharrou          #+#    #+#             */
-/*   Updated: 2019/07/23 20:31:24 by akharrou         ###   ########.fr       */
+/*   Updated: 2019/07/23 23:00:27 by akharrou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,12 @@
 
 #include <sstream>
 #include <iostream>
+#include <ios>
 #include <string>
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+#define ISDIGIT(c) (c >= '0' && c <= '9')
 
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
@@ -26,49 +31,66 @@ int	main(int ac, char **av) {
 		return (1);
 	}
 
-    char	var_char   = 0;
-    int		var_int    = 0;
-    float	var_float  = 0;
-    double	var_double = 0;
+	std::string param(av[1]);
+	int i = param.find('.');
+	int decimals = 0;
+
+	/* Get Number of Decimals (default will be 1) */
+	if (i != -1) {
+		while ( param[++i] && ISDIGIT(param[i]) )
+			  ++decimals;
+	} else {
+		decimals = 1;
+	}
+
+	char	var_char;
+	int		var_int;
+    float	var_float;
+    double	var_double;
+
+	std::cout.precision(decimals);
+	std::cout.setf( std::ios::fixed, std:: ios::floatfield );
 
 	try {
 
-		var_char = static_cast <char> (std::stoi(av[1]));
+		var_char = static_cast <char> (std::stoi(param));
 
-		if (var_char < 32 || 126 < var_char) {
+		if (std::stoi(param) > 127) {
+			std::cout << "char: impossible\n";
+		} else if (var_char < 32 || 126 < var_char) {
 			std::cout << "char: Non displayble\n";
 		} else {
 			std::cout << "char: " << var_char << "\n";
 		}
 
-	} catch( const std::exception & e ) {
+	} catch( const std::exception ) {
 		std::cout << "char: impossible\n";
 	}
 
 	try {
 
-        var_int = static_cast <int> (std::stoi(av[1]));
+		var_int = static_cast <int> (std::stoi(param));
 		std::cout << "int: " << var_int << "\n";
 
-	} catch( const std::exception & e ) {
+	} catch( const std::exception ) {
 		std::cout << "int: impossible\n";
 	}
 
 	try {
 
-        var_float = static_cast <float> (std::stof(av[1]));
-		std::cout << "float: " << var_float << "\n";
+		var_float = (std::stof(param));
+		std::cout << "float: " << var_float << "f\n";
 
-	} catch( const std::exception & e ) {
+	} catch( const std::exception ) {
 		std::cout << "float: impossible\n";
 	}
 
 	try {
 
-        var_double = static_cast <double> (std::stod(av[1]));
+		var_double = (std::stod(param));
 		std::cout << "double: " << var_double << "\n";
 
-	} catch( const std::exception & e ) {
+	} catch( const std::exception ) {
 		std::cout << "double: impossible\n";
 	}
 
