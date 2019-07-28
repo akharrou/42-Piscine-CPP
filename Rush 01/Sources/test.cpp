aded89b76338b8ec78491e75584e428423c8b91d
Aymen Kh <idev.aymen@gmail.com>
2019-07-28 00:06:27 -0700

    Latest Update

--- Rush 01/Sources/test.cpp
+++ Rush 01/Sources/test.cpp
@@ -1,89 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akharrou <akharrou@student.42.us.org>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/07/28 00:05:50 by akharrou          #+#    #+#             */
+/*   Updated: 2019/07/28 00:05:57 by akharrou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include <iostream>
+#include <fstream>
+#include <chrono>
+#include <ctime>
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
 #include <stdlib.h>
 #include <unistd.h>
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
 #include <sys/utsname.h>
 #include <sys/types.h>
 #include <sys/sysctl.h>
 
-#include <fstream>
-#include <chrono>
-#include <ctime>
-
-#include <cpuid.h>
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
 using namespace std;
 using namespace std::chrono;
 
-#include <mach/mach_init.h>
-#include <mach/mach_error.h>
-#include <mach/mach_host.h>
-#include <mach/vm_map.h>
-
-static unsigned long long _previousTotalTicks = 0;
-static unsigned long long _previousIdleTicks = 0;
-
-float CalculateCPULoad(unsigned long long idleTicks, unsigned long long totalTicks)
-{
-  unsigned long long totalTicksSinceLastTime = totalTicks-_previousTotalTicks;
-  unsigned long long idleTicksSinceLastTime  = idleTicks-_previousIdleTicks;
-  float ret = 1.0f-((totalTicksSinceLastTime > 0) ? ((float)idleTicksSinceLastTime)/totalTicksSinceLastTime : 0);
-  _previousTotalTicks = totalTicks;
-  _previousIdleTicks  = idleTicks;
-  return ret;
-}
-
-// Returns 1.0f for "CPU fully pinned", 0.0f for "CPU idle", or somewhere in between
-// You'll need to call this at regular intervals, since it measures the load between
-// the previous call and the current one.
-float GetCPULoad()
-{
-   host_cpu_load_info_data_t cpuinfo;
-   mach_msg_type_number_t count = HOST_CPU_LOAD_INFO_COUNT;
-   if (host_statistics(mach_host_self(), HOST_CPU_LOAD_INFO, (host_info_t)&cpuinfo, &count) == KERN_SUCCESS)
-   {
-      unsigned long long totalTicks = 0;
-      for(int i=0; i<CPU_STATE_MAX; i++) totalTicks += cpuinfo.cpu_ticks[i];
-      return CalculateCPULoad(cpuinfo.cpu_ticks[CPU_STATE_IDLE], totalTicks);
-   }
-   else return -1.0f;
-}
-
-static double ParseMemValue(const char * b)
-{
-   while((*b)&&(isdigit(*b) == false)) b++;
-   return isdigit(*b) ? atof(b) : -1.0;
-}
-
-// Returns a number between 0.0f and 1.0f, with 0.0f meaning all RAM is available, and 1.0f meaning all RAM is currently in use
-float GetSystemMemoryUsagePercentage()
-{
-   FILE * fpIn = popen("/usr/bin/vm_stat", "r");
-   if (fpIn)
-   {
-      double pagesUsed = 0.0, totalPages = 0.0;
-      char buf[512];
-      while(fgets(buf, sizeof(buf), fpIn) != NULL)
-      {
-         if (strncmp(buf, "Pages", 5) == 0)
-         {
-            double val = ParseMemValue(buf);
-            if (val >= 0.0)
-            {
-               if ((strncmp(buf, "Pages wired", 11) == 0)||(strncmp(buf, "Pages active", 12) == 0)) pagesUsed += val;
-               totalPages += val;
-            }
-         }
-         else if (strncmp(buf, "Mach Virtual Memory Statistics", 30) != 0) break;  // Stop at "Translation Faults", we don't care about anything at or below that
-      }
-      pclose(fpIn);
-
-      if (totalPages > 0.0) return (float) (pagesUsed/totalPages);
-   }
-   return -1.0f;  // indicate failure
-}
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
 int main() {
 
