2eb277faa1566b5653608f9f9847e8e622ad2047
Aymen Kh <idev.aymen@gmail.com>
2019-08-07 19:16:07 -0700

    Latest Update

--- Rush 00/Includes/Socket.hpp
+++ Rush 00/Includes/Socket.hpp
@@ -6,7 +6,7 @@
 /*   By: akharrou <akharrou@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/01 17:33:37 by akharrou          #+#    #+#             */
-/*   Updated: 2019/08/07 19:13:23 by akharrou         ###   ########.fr       */
+/*   Updated: 2019/08/07 19:16:02 by akharrou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -111,13 +111,18 @@ class Socket {
 
 	/* STATICS - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
-		static std::string getip   ( struct sockaddr_storage address );
+		static
+        struct addrinfo * getaddrinfo ( const char * Host , const char * Port ,
+                                        int Family   = AF_UNSPEC   ,
+                                        int Type     = DFLT_FAMILY ,
+                                        int Protocol = DFLT_TYPE   ,
+                                        int Flags    = AI_DEFAULT );
 
-		static std::string getport ( struct sockaddr_storage address );
+		static
+        std::string getip     ( struct sockaddr_storage address );
 
-		static struct addrinfo * getaddrinfo ( const char * Host, const char * Port,
-		    int Family = AF_UNSPEC, int Type = DFLT_FAMILY, int Protocol = DFLT_TYPE,
-		    int Flags = AI_DEFAULT );
+		static
+        std::string getport   ( struct sockaddr_storage address );
 
 	/* OPTIONS - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
@@ -125,8 +130,8 @@ class Socket {
 		Socket &   settimeout ( double timeout );
 		Socket &  setblocking ( bool value );
 
-		int	       getsockopt ( int option, int level ) const;
-		double	   gettimeout ( void ) const;
+		int        getsockopt ( int option, int level ) const;
+		double     gettimeout ( void ) const;
 
 	/* OPERATIONS - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
